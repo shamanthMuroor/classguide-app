45c20c3adf854bd0cdaa78e491a167e7737cf505
ShamanthMuroor <shamanthm3@gmail.com>
2019-05-15 13:06:26 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-'
 
+class App extends React.Component {
+    render() 
+        return (
+            <div>
+                <Nav />
+                "hello"
+            </div>
+        )
+}
+export default App;
