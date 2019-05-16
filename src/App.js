1fe68bfc352f5487675ac357351a223eddbc7671
ShamanthMuroor <shamanthm3@gmail.com>
2019-05-16 09:32:30 +0530

    rm google api

--- src/App.js
+++ src/App.js
@@ -14,13 +14,7 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB7pv2G_wvp1Nnb9_Ql48xDsC6nPYTGrvE",
-    authDomain: "class-guide-system.firebaseapp.com",
-    databaseURL: "https://class-guide-system.firebaseio.com",
-    projectId: "class-guide-system",
-    storageBucket: "class-guide-system.appspot.com",
-    messagingSenderId: "252911290527",
-    appId: "1:252911290527:web:c91326363afcce28"
+
   };
 firebase.initializeApp(firebaseConfig);
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,26 +3,35 @@ import '../styles/login.css';
 
 class Login extends React.Component {
   render() {
-    return (
+    return ( 
       <div>
-          <div className="align-center container">
-         <div className="row justify-content-center">
-             <div className="col-md-offset-5 col-md-3">
-                 <div className="form-login">
-                 <h4>Login</h4>
-                 <input type="text" id="userName" className="form-control input-sm chat-input" placeholder="username" />
-                 <br/>
-                 <input type="text" id="userPassword" className="form-control input-sm chat-input" placeholder="password" />
-                 <br/>
-                 <div className="wrapper">
-                 <span className="group-btn">     
-                     <a href="#" className="btn btn-primary btn-md">login <i className="fa fa-sign-in"></i></a>
-                 </span>
-                 </div>
-                 </div>
-             </div>
-         </div>
-     </div>
+      <div class="container-fluid">
+        <img src="http://staloysiuscollege.co.in/newDesign/images/about-aloysius.png" id="bg-img" alt="" />
+        <div class="container">
+            <img src="https://www.sac-aimit.in/epitome/images/aloylogo.png" width="20px" id="logo" alt="" />
+        </div>
+      </div>
+
+      <div>
+        <div className="align-center container">
+          <div className="row justify-content-center">
+            <div className="col-md-offset-5 col-md-3">
+                <div className="form-login">
+                  <h4>Login</h4>
+                  <input type="text" id="userName" className="form-control input-sm chat-input" placeholder="username" />
+                  <br/>
+                  <input type="text" id="userPassword" className="form-control input-sm chat-input" placeholder="password" />
+                  <br/>
+                  <div className="wrapper">
+                    <span className="group-btn">     
+                        <a href="#" className="btn btn-primary btn-md">login <i className="fa fa-sign-in"></i></a>
+                    </span>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
