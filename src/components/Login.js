c8b7e1a4680d37ffc719f0d6f88a8dfbc5eded8b
ShamanthMuroor <shamanthm3@gmail.com>
2019-05-16 08:58:58 +0530

    Style to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import '../styles/login.css';
 
 class Login extends React.Component {
   render() {
     return (
       <div>
-          <div class="align-center container">
-         <div class="row justify-content-center">
-             <div class="col-md-offset-5 col-md-3">
-                 <div class="form-login">
+          <div className="align-center container">
+         <div className="row justify-content-center">
+             <div className="col-md-offset-5 col-md-3">
+                 <div className="form-login">
                  <h4>Login</h4>
-                 <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username" />
-                 </br>
-                 <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
-                 </br>
-                 <div class="wrapper">
-                 <span class="group-btn">     
-                     <a href="#" class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></a>
+                 <input type="text" id="userName" className="form-control input-sm chat-input" placeholder="username" />
+                 <br/>
+                 <input type="text" id="userPassword" className="form-control input-sm chat-input" placeholder="password" />
+                 <br/>
+                 <div className="wrapper">
+                 <span className="group-btn">     
+                     <a href="#" className="btn btn-primary btn-md">login <i className="fa fa-sign-in"></i></a>
                  </span>
                  </div>
                  </div>
