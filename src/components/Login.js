295eddf753f4293afd9cc992572dee931c6e7f0b
Amruthesh <amruth9398@gmail.com>
2019-05-17 23:03:53 +0530

    styled the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,18 +6,34 @@ class Login extends React.Component {
     return (
         <div className="login-container">
             <div className="logo-container">
-                <img src={this.props.logoImg} alt="logo"/>
+                <div className="login-desktop-container">
+                <img src={this.props.logoImg} alt="logo" />
+                <div className="form-container" id="form-desktop">
+                    <form>
+                        <div className="form-group">
+                            <input type="name" className="form-control" placeholder="Username" />
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Password" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">login</button>
+                    </form>
+                </div>
+                
+                </div>
+                
+
             </div>
-            <div className="form-container">
-               <form>
-                   <div className="form-group">
-                        <input type="name" className="form-control" placeholder="Username"/>
-                   </div>
-                   <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password"/>
-                   </div>
-                   <button type="submit" className="btn btn-primary">login</button>
-               </form>
+            <div className="form-container" id="form-mobile">
+                <form>
+                    <div className="form-group">
+                        <input type="name" className="form-control" placeholder="Username" />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">login</button>
+                </form>
             </div>
         </div>
       
