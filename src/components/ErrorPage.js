d858766b6dcff81c88cf3af6262fdaeb46671eb3
hashelse <hashelse@gmail.com>
2019-05-20 17:29:34 +0530

    minor:font changed

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -5,10 +5,10 @@ import './ErrorPage.css'
 class ErrorPage extends React.Component {
   render() {  
     return ( 
-      <div className="container card profile" style={{backgroundColor:'#333333',color:'darkgray',textAlign:'center',fontFamily:'monospace'}}>
+      <div className="container card profile" style={{backgroundColor:'#333333',color:'darkgray',textAlign:'center'}}>
         <div className="card-body">
-            <h1 className="text-flicker-in-glow">404 Error</h1>
-            <p>Sorry, we can’t seem to find what you’re looking for. </p>
+            <h1 className="text-flicker-in-glow text-monospace">404 Error</h1>
+            <p className="">Sorry, we can’t seem to find what you’re looking for. </p>
         </div>
       </div>
     )
