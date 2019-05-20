133ed89b4969e3efd148cafd63e5a49d55c23944
hashelse <hashelse@gmail.com>
2019-05-20 17:36:55 +0530

    minor change

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -8,7 +8,7 @@ class ErrorPage extends React.Component {
       <div className="container card profile" style={{backgroundColor:'#333333',color:'darkgray',textAlign:'center'}}>
         <div className="card-body">
             <h1 className="text-flicker-in-glow text-monospace">404 Error</h1>
-            <p className="">Sorry, we can’t seem to find what you’re looking for. </p>
+            <p>Sorry, we can’t seem to find what you’re looking for. </p>
         </div>
       </div>
     )
