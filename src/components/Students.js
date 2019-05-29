598c2af99b2caf18b731c6ca7c245965064a8dc7
Amruthesh <amruth9398@gmail.com>
2019-05-29 11:37:29 +0530

    class meeting ui

--- src/components/Students.js
+++ src/components/Students.js
@@ -3,7 +3,7 @@ import StudList from './students/StudList';
 import {db} from '../App';
 import Search from './Search';
 import Tags from './Tags';
-import axios from 'axios';
+// import axios from 'axios';
 
 class Students extends React.Component {
     state = { 
@@ -12,39 +12,39 @@ class Students extends React.Component {
         loading: true
     }
 
-    componentWillMount = () => {
-        let arr = [];
-        let newArr = [];
-        axios.get("https://globaldb.sionasolutions.com")
-            .then(value => {
-                value.data.data.forEach((val, i) => {
-                    arr.push(val.course + val.Batch)
-                })
+    // componentWillMount = () => {
+        // let arr = [];
+        // let newArr = [];
+        // axios.get("https://globaldb.sionasolutions.com")
+        //     .then(value => {
+        //         value.data.data.forEach((val, i) => {
+        //             arr.push(val.course + val.Batch)
+        //         })
                 
                 //Removing duplicate array
-                newArr = arr.filter(function (item, i, ar) { return ar.indexOf(item) === i; });
+    //             newArr = arr.filter(function (item, i, ar) { return ar.indexOf(item) === i; });
 
-                db.collection("staff").where("student_id", "==", newArr[12]).get()
-                    .then(value => {
-                        let val = ''
-                        value.forEach(doc => {
-                            val = doc.data().student_id;
-                        })
-                        console.log(val);
-                        db.collection("students").where("_id", "==", val).get()
-                            .then(querySnap => {
-                                querySnap.forEach(val => {
-                                    let exarr = [];
-                                    exarr.push({
-                                        ...val.data()
-                                    })
-                                    this.setState({ studs: this.state.studs.concat(exarr), loading: false })
-                                })
-                            })
-                    })
-            })
-            .catch(err => console.log(err));
-    }
+    //             db.collection("staff").where("student_id", "==", newArr[12]).get()
+    //                 .then(value => {
+    //                     let val = ''
+    //                     value.forEach(doc => {
+    //                         val = doc.data().student_id;
+    //                     })
+    //                     console.log(val);
+    //                     db.collection("students").where("_id", "==", val).get()
+    //                         .then(querySnap => {
+    //                             querySnap.forEach(val => {
+    //                                 let exarr = [];
+    //                                 exarr.push({
+    //                                     ...val.data()
+    //                                 })
+    //                                 this.setState({ studs: this.state.studs.concat(exarr), loading: false })
+    //                             })
+    //                         })
+    //                 })
+    //         })
+    //         .catch(err => console.log(err));
+    // }
 
     componentWillUnmount = () => {
         this.setState({studs: []})
